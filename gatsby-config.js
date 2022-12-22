c1c89937ca3dd75568544a364ceca1219c7a966b
tonyveli <84784060+tonyveli@users.noreply.github.com>
2022-12-22 16:54:19 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Tarenzi Sky View`,
     author: {
-      name: `Kyle Mathews`,
+      name: `L.C.`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `skeyevuewphc`,
     },
   },
   plugins: [
